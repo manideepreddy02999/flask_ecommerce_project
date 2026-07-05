5b011105a9c6e07510c18e745cb0a09f2b94573d
manideepreddy <manideepreddytippana@gmail.com>
2026-07-06 00:03:08 +0530

    pdn change

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
-
 import os
+from dotenv import load_dotenv
+
+# Explicitly load .env file for production environments like PythonAnywhere
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     PERMANENT_SESSION_LIFETIME = os.getenv('PERMANENT_SESSION_LIFETIME')
